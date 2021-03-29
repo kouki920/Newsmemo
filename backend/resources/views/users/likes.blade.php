60019d8950b6ee0a8335f9c833a52d936146140f
kouki920 <kouki.htt.920@gmail.com>
2021-03-29 12:16:46 +0900

    [add]ユーザー詳細ページのbladeを共通化
    いいね一覧と投稿一覧での共通箇所(プロフィール部分)をusers/user.blade.phpで管理
    いいねした投稿一覧はlikes.blade.phpで管理

--- backend/resources/views/users/likes.blade.php
+++ backend/resources/views/users/likes.blade.php
@@ -1,6 +1,6 @@
 @extends('app')
 
-@section('title', $user->name)
+@section('title', $user->name . 'がいいねした記事')
 
 @section('content')
 @include('nav')
@@ -8,12 +8,12 @@
     @include('users.user')
     <ul class="nav nav-tabs nav-justified mt-3">
         <li class="nav-item">
-            <a class="nav-link text-muted active" href="{{ route('users.show', ['name' => $user->name]) }}">
+            <a class="nav-link text-muted" href="{{ route('users.show', ['name' => $user->name]) }}">
                 記事
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link text-muted" href="{{route('users.likes',['name' => $user->name])}}">
+            <a class="nav-link text-muted active" href="{{ route('users.likes', ['name' => $user->name]) }}">
                 いいね
             </a>
         </li>
