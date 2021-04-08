e2ddd90f0dea92ab0a473f62aec9edabc1449958
kouki920 <kouki.htt.920@gmail.com>
2021-04-08 17:31:36 +0900

    [add]NEWS API関連機能の追記
    NEWS APIを使いデータを取得後、一覧で表示させる機能を追加
    ニュースを国別、カテゴリ別で取得できる機能を追加
    タブを作成し、ニュース一覧画面と投稿一覧画面を遷移できる機能を追加

--- backend/resources/views/articles/index.blade.php
+++ backend/resources/views/articles/index.blade.php
@@ -5,6 +5,7 @@
 @section('content')
 @include('nav')
 <div class="container">
+    @include('articles.tabs', ['hasNewsApi' => false, 'hasArticles' => true])
     @foreach($articles as $article)
     @include('articles.post')
     @endforeach
---
+++ backend/resources/views/articles/news.blade.php
@@ -0,0 +1,8 @@
+<div class="card-body pt-0 pb-2">
+    <h3 class="h5 card-title">
+        <a href="{{$data['url']}}">{{$data['name']}}</a>
+    </h3>
+    <div class="card-text">
+        <img src="{{$data['thumbnail']}}">
+    </div>
+</div>
