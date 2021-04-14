2631931139b90f3879f09b98c3126325906f1165
kouki920 <kouki.htt.920@gmail.com>
2021-04-14 16:58:07 +0900

    [update]後で読む欄の画面でトップページのタグを表示するように修正

--- backend/resources/views/users/likes.blade.php
+++ backend/resources/views/users/likes.blade.php
@@ -5,6 +5,7 @@
 @section('content')
 @include('nav')
 <div class="container">
+    @include('articles.tabs', ['hasNewsApi' => false, 'hasCovidNews' => false,'hasArticles' => false,'hasMypage' => true])
     @include('users.profile')
     @include('users.tabs', ['hasArticles' => false, 'hasLikes' => true])
     @foreach($articles as $article)
