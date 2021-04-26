146362972f2e61452baeb3f5e62757af23305a69
kouki920 <kouki.htt.920@gmail.com>
2021-04-26 11:12:38 +0900

    [add]vue用のfontawesomeをインストール
    app.jsで設定

--- backend/resources/js/app.js
+++ backend/resources/js/app.js
@@ -7,9 +7,18 @@
 import './bootstrap';
 import './fontawesome';
 import Vue from 'vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import ArticleLike from './components/ArticleLike';
 import ArticleTagsInput from './components/ArticleTagsInput';
 import FollowButton from './components/FollowButton';
+
+library.add(faUserSecret)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.config.productionTip = false
 
 const app = new Vue({
   el: '#app',
