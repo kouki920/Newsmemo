9782c8354b8bdb511b170a13acb6e156bcef2618
kouki920 <kouki.htt.920@gmail.com>
2021-04-27 21:30:11 +0900

    [add]メモとニュースのランキング表示機能を追加
    メモ:ブクマされた数が多い順に上位3位までランキング表示
    ニュース:メモされた数が多い順に上位3位までランキング表示

--- backend/app/Models/Article.php
+++ backend/app/Models/Article.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Support\Facades\DB;
 
 class Article extends Model
 {
@@ -36,5 +37,25 @@ class Article extends Model
     public function tags(): BelongsToMany
     {
         return $this->belongsToMany('App\Models\Tag')->withTimestamps();
+    }
+
+    /**
+     * 後で読むボタンを押された数が多い順にメモデータを取得する
+     */
+    public function articleRanking()
+    {
+        $ranked_article = Article::withCount('likes')->orderBy('likes_count', 'desc')->limit(3)->get();
+
+        return $ranked_article;
+    }
+
+    /**
+     * よく読まれているニュースを取得する
+     */
+    public function newsRanking()
+    {
+        $news = Article::select('url', 'news', DB::raw('count(*) as total'))->groupBy('url', 'news')->having('total', '>', 1)->orderBy('total', 'desc')->limit(3)->get();
+
+        return $news;
     }
 }
