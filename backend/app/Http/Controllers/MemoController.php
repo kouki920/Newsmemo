42b03ec9254b520ce888479beb562ac135c4b008
kouki920 <kouki.htt.920@gmail.com>
2021-05-05 23:44:16 +0900

    [add]メモ追記機能実装
    未完成(ルーティングとコントローラの作成)

---
+++ backend/app/Http/Controllers/MemoController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Article;
+
+class MemoController extends Controller
+{
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function edit()
+    {
+        //
+    }
+
+    public function update()
+    {
+        //
+    }
+
+    public function destroy()
+    {
+        //
+    }
+}
