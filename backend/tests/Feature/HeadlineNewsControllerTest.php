8a34738124f87959a92b9960c49456cfa911caf3
kouki920 <kouki.htt.920@gmail.com>
2021-05-22 10:12:17 +0900

    [add]テスト実装
    その他コード修正

--- backend/tests/Feature/HeadlineNewsControllerTest.php
+++ backend/tests/Feature/HeadlineNewsControllerTest.php
@@ -2,21 +2,25 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class HeadlineNewsControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    ### ヘッドラインニュース一覧表示のテスト ###
+
+    // ログイン時
+    public function testCovidNewsIndex()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)->get(route('news.default_index'));
+
+        $response->assertStatus(200)
+            ->assertViewIs('articles.news_index');
     }
 }
