846677e72c8a040b0fff1eb99053941ffbae4322
kouki920 <kouki.htt.920@gmail.com>
2021-07-30 18:06:02 +0900

    [update]テーブルのデータ型等の修正

--- backend/database/factories/ContactFactory.php
+++ backend/database/factories/ContactFactory.php
@@ -15,6 +15,6 @@ $factory->define(Contact::class, function (Faker $faker) {
         'gender' => $faker->randomElement($array = ['男性', '女性', '無回答']),
         'email' => Str::random(15) . '@sample.com',
         'age' => $faker->randomElement(['10', '20', '30', '40', '50', '60', '70', '80', '90']),
-        'content' => $faker->text(255),
+        'content' => 'テスト',
     ];
 });
--- backend/resources/views/users/data_index.blade.php
+++ backend/resources/views/users/data_index.blade.php
@@ -3,21 +3,13 @@
         <tr class="table-active text-center">
             <th scope="col"><i class="fas fa-sticky-note fa-fw"></i>メモ数</th>
             <th scope="col"><i class="far fa-chart-bar fa-fw"></i>累計投稿日数</th>
+            <th scope="col"><i class="far fa-clock fa-fw"></i>最終ログイン</th>
         </tr>
     </thead>
     <tbody>
         <tr class="text-center">
             <td>{{$articles_count}}個</td>
             <td>{{$days_posted}}日</td>
-        </tr>
-    </tbody>
-    <thead>
-        <tr class="table-active text-center">
-            <th scope="col"><i class="far fa-clock fa-fw"></i>最終ログイン</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr class="text-center">
             <td>{{$last_login}}</td>
         </tr>
     </tbody>
