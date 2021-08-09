4131ae256ab3149dd12b837f62785796f8c07096
kouki920 <kouki.htt.920@gmail.com>
2021-08-09 14:16:17 +0900

    [update]ニュース記事のレイアウト修正

--- backend/resources/views/articles/news.blade.php
+++ backend/resources/views/articles/news.blade.php
@@ -1,17 +1,17 @@
 <div class="card mt-2 mb-4">
     @if(isset($data['thumbnail']))
-    <img src="{{$data['thumbnail']}}" class="card-img-top img-fluid img-thumbnail" alt="NEWSAPIのサムネイル" />
+    <img src="{{$data['thumbnail']}}" class="card-img-top img-fluid img-thumbnail news-thumbnail" alt="NEWSAPIのサムネイル" />
     @else
-    <img src="{{asset('/assets/images/noimage.png')}}" class="card-img-top img-fluid img-thumbnail" alt="Noimage" />
+    <img src="{{asset('/assets/images/noimage.png')}}" class="card-img-top img-fluid img-thumbnail news-thumbnail" alt="Noimage" />
     @endif
     <div class="card-body">
-        <h5 class="card-title"><a href="{{$data['url']}}" target=”_blank” rel="noopener noreferrer">{{$data['name']}}
+        <h5 class="card-title news-title font-md"><a class="news-url" href="{{$data['url']}}" target=”_blank” rel="noopener noreferrer">{{$data['name']}}
             </a></h5>
         <form action="{{route('articles.create')}}" method="POST">
             @csrf
             <input type="hidden" name="news" value="{{$data['name']}}">
             <input type="hidden" name="url" value="{{$data['url']}}">
-            <input type="submit" value="メモする">
+            <input type="submit" value="メモ">
         </form>
         <form action="{{route('articles.store')}}" method="POST">
             @csrf
