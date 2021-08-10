0d2e48c7972d4b13c10efbef01ce6adad5ede8aa
kouki920 <kouki.htt.920@gmail.com>
2021-08-10 14:37:29 +0900

    [update]ニュース記事のレイアウトを修正

--- backend/resources/views/articles/news_tabs.blade.php
+++ backend/resources/views/articles/news_tabs.blade.php
@@ -1,33 +1,35 @@
-    <form class="form-inline" method="post" action="{{route('news.custom_index')}}">
-        @csrf
-        <div class="form-group select-box">
-            <!-- 国名: -->
-            <select name="country">
-                <option disabled selected value>国名</option>
-                <option value="jp">日本</option>
-                <option value="fr">フランス</option>
-                <option value="ca">カナダ</option>
-                <option value="de">ドイツ</option>
-                <option value="gb">イギリス</option>
-                <option value="it">イタリア</option>
-                <option value="us">アメリカ</option>
-                <option value="ru">ロシア</option>
-            </select>
-        </div>
-        <div class="form-group select-box">
-            <!-- カテゴリー: -->
-            <select name="category">
-                <option disabled selected value>カテゴリー</option>
-                <option value="entertainment">entertainment</option>
-                <option value="business">business</option>
-                <option value="general">general</option>
-                <option value="health">health</option>
-                <option value="science">science</option>
-                <option value="sports">sports</option>
-                <option value="technology">technology</option>
-            </select>
-        </div>
-        <div class="form-group update-button">
-            <input id="update-button" type="submit" value="更新">
-        </div>
-    </form>
+    <div class="select-box-body">
+        <form class="form-inline" method="post" action="{{route('news.custom_index')}}">
+            @csrf
+            <div class="form-group select-box">
+                <!-- 国名: -->
+                <select name="country">
+                    <option disabled selected value>国名</option>
+                    <option value="jp">日本</option>
+                    <option value="fr">フランス</option>
+                    <option value="ca">カナダ</option>
+                    <option value="de">ドイツ</option>
+                    <option value="gb">イギリス</option>
+                    <option value="it">イタリア</option>
+                    <option value="us">アメリカ</option>
+                    <option value="ru">ロシア</option>
+                </select>
+            </div>
+            <div class="form-group select-box">
+                <!-- カテゴリー: -->
+                <select name="category">
+                    <option disabled selected value>カテゴリー</option>
+                    <option value="entertainment">芸能</option>
+                    <option value="business">ビジネス</option>
+                    <option value="general">一般</option>
+                    <option value="health">健康</option>
+                    <option value="science">サイエンス</option>
+                    <option value="sports">スポーツ</option>
+                    <option value="technology">テクノロジー</option>
+                </select>
+            </div>
+            <div class="form-group update-button">
+                <input id="update-button" type="submit" value="更新">
+            </div>
+        </form>
+    </div>
