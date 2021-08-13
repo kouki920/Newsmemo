0072ff63066de26d4db8d1cd9b24a663ac0ae43b
kouki920 <kouki.htt.920@gmail.com>
2021-08-13 15:45:16 +0900

    [update]投稿一覧画面のレイアウト修正

--- backend/resources/views/articles/search.blade.php
+++ backend/resources/views/articles/search.blade.php
@@ -1,7 +1,9 @@
-<form class="form-inline ml-3 mt-lg-3">
-    @csrf
-    <input class="form-control mr-sm-2" type="search" name="search" value="{{request('search')}}" placeholder="検索" aria-label="Search">
+<div class="search-form">
+    <form class="form-inline">
+        @csrf
+        <input class="form-control search-content font-sm" type="search" name="search" value="{{request('search')}}" placeholder="検索" aria-label="Search">
 
-    <button class="btn btn-outline-success ml-2 my-sm-0" type="submit">検索</button>
+        <button class="btn search-button font-sm" type="submit">検索</button>
 
-</form>
+    </form>
+</div>
--- backend/resources/views/sidebar/articles_ranking.blade.php
+++ backend/resources/views/sidebar/articles_ranking.blade.php
@@ -1,11 +1,11 @@
 <div class="card-body articles-ranking-body">
     <ul class="list-group">
-        <li class="list-group-item bg-info text-center articles-ranking-title">トレンドメモTOP3</li>
+        <li class="list-group-item articles-ranking-title font-sm">トレンドメモTOP3</li>
     </ul>
     @foreach($ranked_articles as $article)
     <div class="list-group">
-        <a href="{{route('articles.show',compact('article'))}}" class="list-group-item list-group-item-action text-center articles-ranking-content">
-            <p class="h6">{!! nl2br(e(Str::limit($article->body,60))) !!}<br><i class="fas fa-bookmark fa-fw"></i>{{$article->likes_count}}件</p>
+        <a href="{{route('articles.show',compact('article'))}}" class="list-group-item list-group-item-action articles-ranking-content">
+            <p class="font-sm">{!! nl2br(e(Str::limit($article->body,60))) !!}<br><i class="fas fa-bookmark fa-fw"></i>{{$article->likes_count}}件</p>
         </a>
     </div>
     @endforeach
