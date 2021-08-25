4ddb36faf1de8d285ae1eb849f264fae3eb9aa8d
kouki920 <kouki.htt.920@gmail.com>
2021-08-25 14:29:01 +0900

    [update]マイページと投稿レイアウトの修正

--- backend/resources/views/users/profile.blade.php
+++ backend/resources/views/users/profile.blade.php
@@ -1,7 +1,7 @@
     <div class="card user-profile-body">
         <div class="card-body">
             <!-- ユーザーアイコン -->
-            <div class="d-flex flex-row">
+            <div class="profile-function-button">
                 @if(Auth::id() == config('user.guest_user_id'))
                 @if(!isset($user->image))
                 <img src="{{asset('/assets/images/noicon.jpeg')}}" class="profile-icon image-upload rounded-circle img-responsive mr-1" width="55" height="55" alt="Noicon">
@@ -25,52 +25,52 @@
                 @endif
 
                 <!-- タグ表示 -->
-                <div class="row">
-                    <div class="card-text col">
-                        <div class="dropdown">
-                            <a data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <button class="btn recent-tags-btn dropdown-toggle font-sm" type="button" id="dropdownMenuButton" data-mdb-toggle="dropdown" aria-expanded="false">
-                                    最近使用したタグ
-                                </button>
-                            </a>
-                            <div class="dropdown-menu dropdown-menu-right category-dropdown-menu">
-                                <div>
-                                    @foreach($total_category as $category)
-                                    <ul class="list-group">
-                                        <li class="list-group-item h6">{{$category}}</li>
-                                    </ul>
-                                    @endforeach
-                                </div>
+                <div class="card-text col">
+                    <div class="dropdown">
+                        <a data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <button class="btn recent-tags-btn dropdown-toggle font-sm" type="button" id="dropdownMenuButton" data-mdb-toggle="dropdown" aria-expanded="false">
+                                最近使用したタグ
+                            </button>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right category-dropdown-menu">
+                            <div>
+                                @foreach($total_category as $category)
+                                <ul class="list-group">
+                                    <li class="list-group-item h6">{{$category}}</li>
+                                </ul>
+                                @endforeach
                             </div>
                         </div>
                     </div>
-
-                    <!-- 編集ボタン -->
-                    @if(Auth::id() == $user->id)
-                    <div class="card-text col">
-                        <a href="{{route('users.edit',['name' => $user->name])}}"><input type="button" class="font-sm btn edit-button" value="編集"></a>
-                    </div>
-                    @endif
                 </div>
+
+                <!-- 編集ボタン -->
+                @if(Auth::id() == $user->id)
+                <div class="card-text col row">
+                    <a href="{{route('users.edit',['name' => $user->name])}}"><input type="button" class="font-sm btn edit-button" value="編集"></a>
+                </div>
+                @endif
             </div>
 
             <!-- ユーザー名・自己紹介 -->
             <div class="card-body profile-content">
-                <div class="card-text profile-name">
+                <div class="card-text font-md profile-name">
                     {{ $user->name }}
                 </div>
-                <div class="card-text profile-introduction">
+                <div class="card-text font-md profile-introduction">
                     {{$user->introduction}}
                 </div>
 
                 <!-- フォローボタン フォロー・フォロワー数値-->
-                @if(Auth::id() !== $user->id)
-                <follow-button-and-count :initial-is-followed-by='@json($user->isFollowedBy(Auth::user()))' :authorized='@json(Auth::check())' :initial-count-followings='@json($user->count_followings)' :initial-count-followers='@json($user->count_followers)' user-follow="{{ route('users.follow', ['name' => $user->name]) }}" user-follower="{{route('users.follower',['name' => $user->name])}}" user-following="{{route('users.following',['name' => $user->name])}}">
-                </follow-button-and-count>
-                @elseif(Auth::id() == $user->id)
-                <follow-count :authorized='@json(Auth::check())' :initial-count-followings='@json($user->count_followings)' :initial-count-followers='@json($user->count_followers)' user-follow="{{ route('users.follow', ['name' => $user->name]) }}" user-follower="{{route('users.follower',['name' => $user->name])}}" user-following="{{route('users.following',['name' => $user->name])}}">
-                </follow-count>
-                @endif
+                <div class="card-text font-md profile-follow-follower-count">
+                    @if(Auth::id() !== $user->id)
+                    <follow-button-and-count :initial-is-followed-by='@json($user->isFollowedBy(Auth::user()))' :authorized='@json(Auth::check())' :initial-count-followings='@json($user->count_followings)' :initial-count-followers='@json($user->count_followers)' user-follow="{{ route('users.follow', ['name' => $user->name]) }}" user-follower="{{route('users.follower',['name' => $user->name])}}" user-following="{{route('users.following',['name' => $user->name])}}">
+                    </follow-button-and-count>
+                    @elseif(Auth::id() == $user->id)
+                    <follow-count :authorized='@json(Auth::check())' :initial-count-followings='@json($user->count_followings)' :initial-count-followers='@json($user->count_followers)' user-follow="{{ route('users.follow', ['name' => $user->name]) }}" user-follower="{{route('users.follower',['name' => $user->name])}}" user-following="{{route('users.following',['name' => $user->name])}}">
+                    </follow-count>
+                    @endif
+                </div>
             </div>
         </div>
     </div>
