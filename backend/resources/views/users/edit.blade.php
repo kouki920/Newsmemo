008b1461f7247537d775283972f0e803172e4f36
kouki920 <kouki.htt.920@gmail.com>
2021-08-29 19:01:02 +0900

    [update]レイアウト修正

--- backend/resources/views/users/edit.blade.php
+++ backend/resources/views/users/edit.blade.php
@@ -9,7 +9,7 @@
         <div class="mx-auto col col-12 col-sm-11 col-md-9 col-lg-7 col-xl-6">
             <div class="card mt-3">
                 <div class="card-body text-center">
-                    <h2 class="h3 card-title text-center mt-2">プロフィールの編集</h2>
+                    <div class="card-title text-center font-lr">-プロフィールの編集-</div>
 
                     @include('error_list')
                     @if (Auth::id() == config('user.guest_user_id'))
@@ -24,18 +24,18 @@
                             @csrf
                             @method('PATCH')
                             <div class="form-group">
-                                <label for="name">お名前
-                                    <small class="blue-grey-text">（25文字以内）</small>
+                                <label for="name">
+                                    <small class="font-sm">-お名前（25文字以内）-</small>
                                 </label>
                                 @if (Auth::id() == config('user.guest_user_id'))
                                 <input type="text" class="form-control" id="name" name="name" value="{{$user->name}}" readonly>
                                 @else
                                 <input type="text" class="form-control" id="name" name="name" value="{{$user->name ?? old('name')}}">
                                 @endif
-                                <small class="text-muted ">※半角英数字8~16文字以内で入力して下さい</small>
+                                <small class="font-sm">※半角英数字8~16文字以内で入力して下さい</small>
                             </div>
                             <div class="form-group">
-                                <label for="email">メールアドレス</label>
+                                <label for="email font-sm">-メールアドレス-</label>
                                 @if (Auth::id() == config('user.guest_user_id'))
                                 <input type="text" class="form-control" id="email" name="email" value="{{$user->email}}" readonly>
                                 @else
@@ -43,16 +43,16 @@
                                 @endif
                             </div>
                             <div class="form-group">
-                                <label for="introduction">自己紹介
-                                    <small class="blue-grey-text">（200文字以内）</small>
+                                <label for="introduction">
+                                    <small class="font-sm">-自己紹介（200文字以内）-</small>
                                 </label>
                                 <textarea name="introduction" class="form-control" id="introduction" cols="3" rows="3">{{$user->introduction ?? old('introduction')}}</textarea>
                             </div>
                             <div class='btn-toolbar float-right' role="toolbar">
-                                <button class="btn blue-gradient mt-2 mb-2" type="submit">更新</button>
+                                <button class="btn user-profile-update-button font-sm" type="submit">更新</button>
                             </div>
                         </form>
-                        <a href="{{route('users.show',['name' => $user->name])}}"><button class="btn blue-gradient mt-2 mb-2 float-left" type="submit">キャンセル</button></a>
+                        <a href="{{route('users.show',['name' => $user->name])}}"><button class="btn float-left user-profile-cancel-button font-sm" type="submit">キャンセル</button></a>
                     </div>
                 </div>
             </div>
