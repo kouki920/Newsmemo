74437057ff79a3c501a439592b9be85c041dcd92
kouki920 <kouki.htt.920@gmail.com>
2021-09-05 19:07:16 +0900

    [update]レイアウト修正

--- backend/resources/views/users/data_index.blade.php
+++ backend/resources/views/users/data_index.blade.php
@@ -1,15 +1,15 @@
-<table class="table table-bordered mt-4">
+<table class="table table-bordered user-data-body">
     <thead>
-        <tr class="table-active text-center user-data-header">
+        <tr class="table-active user-data-header">
             <th scope="col"><i class="fas fa-sticky-note fa-fw"></i>メモ数</th>
             <th scope="col"><i class="far fa-chart-bar fa-fw"></i>累計投稿日数</th>
             <th scope="col"><i class="far fa-clock fa-fw"></i>最終ログイン</th>
         </tr>
     </thead>
     <tbody>
-        <tr class="text-center user-data-contents">
-            <td>{{$articles_count}}個</td>
-            <td>{{$days_posted}}日</td>
+        <tr class="user-data-contents">
+            <td>{{$articles_count}}</td>
+            <td>{{$days_posted}}</td>
             <td>{{$last_login}}</td>
         </tr>
     </tbody>
