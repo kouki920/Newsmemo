5554b768c73accb4cd4e694db3256dbf1011e132
kouki920 <kouki.htt.920@gmail.com>
2021-10-07 17:08:43 +0900

    [update]scssの修正 #289

--- backend/resources/views/auth/login.blade.php
+++ backend/resources/views/auth/login.blade.php
@@ -17,18 +17,18 @@
 
                 <label class="login-body__item font-md" for="email">-&ensp;e-mail&ensp;-</label>
                 <div class="md-form login-body__form">
-                    <input class="login-body__form__content font-sm" type="text" id="email" name="email" required value="{{ old('email') }}">
+                    <input class="login-body__form-content font-sm" type="text" id="email" name="email" required value="{{ old('email') }}">
                 </div>
 
                 <label class="login-body__item font-md" for="password">-&ensp;password&ensp;-</label>
                 <div class="md-form login-body__form">
-                    <input class="login-body__form__content font-sm" type="password" id="password" name="password" required>
+                    <input class="login-body__form-content font-sm" type="password" id="password" name="password" required>
                 </div>
 
                 <input type="hidden" name="remember" id="remember" value="on">
 
-                <div class="login-password-link">
-                    <a href="{{ route('password.request') }}" class="card-text login-password-link__text font-sm">パスワードを忘れた場合</a>
+                <div class="password-setting-link">
+                    <a href="{{ route('password.request') }}" class="card-text password-setting-link__text font-sm">パスワードを忘れた場合</a>
                 </div>
 
                 <button class="login-button font-sm" type="submit">
