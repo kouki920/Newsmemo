c919c38d824dbef32f73470a1c9b7f07a29124dd
kouki920 <kouki.htt.920@gmail.com>
2021-10-13 23:56:40 +0900

    [update]scssの修正 #289

--- backend/resources/views/articles/post.blade.php
+++ backend/resources/views/articles/post.blade.php
@@ -47,12 +47,12 @@
                     <form method="POST" action="{{ route('articles.destroy', compact('article')) }}">
                         @csrf
                         @method('DELETE')
-                        <div class="modal-body article-delete-text font-sm">
+                        <div class="modal-body article-body__modal-delete-text font-sm">
                             投稿したメモを本当に削除しますか?
                         </div>
-                        <div class="modal-footer justify-content-between">
-                            <a class="btn font-sm article-cancel-button" data-dismiss="modal">キャンセル</a>
-                            <button type="submit" class="btn font-sm article-delete-button">削除</button>
+                        <div class="modal-footer article-body__modal-button">
+                            <a class="btn font-sm article-body__modal-button--cancel" data-dismiss="modal">キャンセル</a>
+                            <button type="submit" class="btn font-sm article-body__modal-button--delete">削除</button>
                         </div>
                     </form>
                 </div>
@@ -78,19 +78,19 @@
         </div>
         @endif
     </div>
-    <div class="card-body article-body-content">
+    <div class="card-body article-body__content">
         <div class="card-text">
-            <a class="font-sm text-dark text-decoration-none" href="{{route('articles.show',compact('article'))}}">
+            <a class="font-sm article-body__content-text" href="{{route('articles.show',compact('article'))}}">
                 {!! nl2br(e( $article->body )) !!}
             </a>
         </div>
     </div>
     @foreach($article->tags as $tag)
     @if($loop->first)
-    <div class="card-body hashtag-body">
+    <div class="card-body article-body__hashtag">
         <div class="card-text line-height">
             @endif
-            <a href="{{ route('tags.show', ['name' => $tag->name]) }}" class="hashtag-text border text-muted font-sm">
+            <a href="{{ route('tags.show', ['name' => $tag->name]) }}" class="article-body__hashtag-text border text-muted font-sm">
                 {{ $tag->hashtag }}
             </a>
             @if($loop->last)
@@ -99,11 +99,11 @@
     @endif
     @endforeach
     @if(isset($article->newsLink))
-    <div class="card-text article-news-link">
-        <a class="font-sm article-news-link-text" href="{{$article->newsLink->url}}" target=”_blank” rel="noopener noreferrer">関連記事:{{$article->newsLink->news}}</a>
+    <div class="card-text article-body__news-link">
+        <a class="font-sm article-body__news-link-text" href="{{$article->newsLink->url}}" target=”_blank” rel="noopener noreferrer">関連記事:{{$article->newsLink->news}}</a>
     </div>
     @endif
-    <div class="card-text article-time font-sm">
+    <div class="card-text article-body__date font-sm">
         {{$article->created_at->format('Y/m/d H:i')}}
     </div>
     <div class="article-async-button card-body">
