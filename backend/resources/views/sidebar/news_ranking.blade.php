199206b9746952edb894b8d4a79d6884168373bb
kouki920 <kouki.htt.920@gmail.com>
2021-10-19 23:14:18 +0900

    [update]scssの修正 #289

--- backend/resources/views/sidebar/news_ranking.blade.php
+++ backend/resources/views/sidebar/news_ranking.blade.php
@@ -1,10 +1,10 @@
 <div class="card-body news-ranking-body">
     <ul class="list-group">
-        <li class="list-group-item news-ranking-title font-sm">トレンドニュースTOP3</li>
+        <li class="list-group-item news-ranking-body__title font-sm">トレンドニュースTOP3</li>
     </ul>
     @foreach($ranked_news as $news)
     <div class="list-group">
-        <a href="{{$news->url}}" class="list-group-item list-group-item-action news-ranking-content" target=”_blank” rel="noopener noreferrer">
+        <a href="{{$news->url}}" class="list-group-item list-group-item-action news-ranking-body__content" target=”_blank” rel="noopener noreferrer">
             <p class="font-sm">{!! nl2br(e(Str::limit($news->news,60))) !!}</p>
         </a>
     </div>
