a8e9f5ca33bc6e96f8a77d5eabc0a2e17591697a
kouki920 <kouki.htt.920@gmail.com>
2021-11-03 23:37:00 +0900

    [update]レイアウトの修正 #304

--- backend/resources/views/sidebar/articles_ranking.blade.php
+++ backend/resources/views/sidebar/articles_ranking.blade.php
@@ -5,7 +5,7 @@
     @foreach($ranked_articles as $article)
     <div class="list-group">
         <a href="{{route('articles.show',compact('article'))}}" class="list-group-item list-group-item-action articles-ranking-body__content">
-            <p class="font-sm">{!! nl2br(e(Str::limit($article->body,60))) !!}<br><i class="fas fa-bookmark fa-fw"></i>{{$article->likes_count}}件</p>
+            <p class="font-sm">{!! nl2br(e(Str::limit($article->body,40))) !!}<br><i class="fas fa-bookmark fa-fw"></i>{{$article->likes_count}}件</p>
         </a>
     </div>
     @endforeach
