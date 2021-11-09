047e12430b71823cf6111551a7306359f6a5c5d2
kouki920 <kouki.htt.920@gmail.com>
2021-11-09 18:19:01 +0900

    [update]レイアウトの修正 表記内容の変更

--- backend/resources/views/memos/add_form.blade.php
+++ backend/resources/views/memos/add_form.blade.php
@@ -1,7 +1,7 @@
     <div class="form-outline memo-body__create-form">
         <form action="{{route('memos.store', compact('article'))}}" method="POST">
             @csrf
-            <textarea class="form-control memo-body__textarea font-sm" id="textAreaExample" rows="3" name="body" placeholder="メモを入力する">{{old('body')}}</textarea>
+            <textarea class="form-control memo-body__textarea font-sm" id="textAreaExample" rows="3" name="body" placeholder="アウトプットを追加">{{old('body')}}</textarea>
             <input type="hidden" name="article_id" value="{{$article->id}}">
             <div class="memo-body__memo-button-wrap">
                 <button type="submit" class="btn memo-body__memo-button font-sm">保存</button>
