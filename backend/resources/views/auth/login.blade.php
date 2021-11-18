7ca3cfe381df5382e9d4cfe18daff36433422202
kouki920 <kouki.htt.920@gmail.com>
2021-11-18 22:56:21 +0900

    [update]ログインページの修正 #282

--- backend/resources/views/auth/login.blade.php
+++ backend/resources/views/auth/login.blade.php
@@ -9,28 +9,10 @@
 
 <div class="container">
     <div class="logo-body">
-        <img src="{{asset('/assets/images/Newsmemo.png')}}" alt="logo" class="logo-body__img">
+        <img src="{{asset('/assets/images/Newsmemo_logo.png')}}" alt="logo" class="logo-body__img">
     </div>
-    <section class="login-introduction-body">
-        <div class="login-introduction-body__text">
-            <p class="font-md">
-                本サービスは、世の中に対する日々の気づきを記録するサービスです。
-                <br>
-                ビジネスマンから学生まで、誰でも簡単にご利用いただけます。
-                <br>
-                まずは会員登録から始めましょう！
-            </p>
-            <p class="font-sm">
-                ※このサービスは、ポートフォリオ用に制作しているものである為
-                <br>
-                現段階では定期的にデータを更新しております。
-                <br>
-                予めご了承ください。
-            </p>
-        </div>
-    </section>
     <div class="login-header-body">
-        <p class="login-header-body__title font-lg">-Login-</p>
+        <p class="login-header-body__title font-lg">-&ensp;Login&ensp;-</p>
     </div>
     <div class="card login-body">
         <div class="card-text">
@@ -51,7 +33,7 @@
                 <input type="hidden" name="remember" id="remember" value="on">
 
                 <div class="password-setting-link">
-                    <a href="{{ route('password.request') }}" class="card-text password-setting-link__text font-sm">パスワードを忘れた場合</a>
+                    <a href="{{ route('password.request') }}" class="card-text password-setting-link__text font-sm">※パスワードを忘れた場合</a>
                 </div>
 
                 <button class="login-button font-sm" type="submit">
@@ -67,6 +49,27 @@
             <div class="user-register-button">
                 <a href="{{ route('register') }}" class="btn-block card-text user-register-button__text font-sm">新規登録</a>
             </div>
+        </div>
+    </div>
+    <div class="subtitle-body">
+        <img src="{{asset('/assets/images/Newsmemo_subtitle.png')}}" alt="logo" class="subtitle-body__img">
+    </div>
+    <div class="login-introduction-body">
+        <div class="login-introduction-body__text">
+            <p class="font-md">
+                本サービスは、世の中に対する日々の気づきを記録するサービスです。
+                <br>
+                ビジネスマンから学生まで、誰でも簡単にご利用いただけます。
+                <br>
+                まずは会員登録から始めましょう！
+            </p>
+            <p class="font-sm login-introduction-body__text--mute">
+                ※このサービスは、ポートフォリオ用に制作しているものである為
+                <br>
+                現段階では定期的にデータを更新しております。
+                <br>
+                予めご了承ください。
+            </p>
         </div>
     </div>
     <div class="fixed-bottom">
