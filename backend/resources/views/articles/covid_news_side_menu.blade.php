fd2c26cd9a5af4b7cfffa7663974ceded206d4ee
kouki920 <kouki.htt.920@gmail.com>
2021-11-29 17:54:20 +0900

    [update]レイアウトの修正 #304

--- backend/resources/views/articles/covid_news_side_menu.blade.php
+++ backend/resources/views/articles/covid_news_side_menu.blade.php
@@ -2,38 +2,56 @@
     <form class="form-inline" method="post" action="{{route('news.covid_custom_index')}}">
         @csrf
         <div class="form-group covid-news-language-menu font-sm">
-            <p class="covid-news-side-menu-title">言語選択</p>
-            <ul class="covid-news-language-list">
-                <li class="covid-news-language-item"><input type="radio" id="jp-option" name="language" value="jp"><label for="jp-option">日本語</label>
-                    <div class="check"></div>
+            <p class="covid-news-side-menu-title">Country</p>
+            <ul class="covid-news-language-list font-sm">
+                <li class="covid-news-language-item"><input type="radio" class="covid-news-language-item__radio" id="jp-option" name="language" value="jp"><label for="jp-option" class="covid-news-language-item__label">
+                        <div class="covid-news-language-item__text">Japan</div>
+                    </label>
+                    <div class="covid-news-language-item__check radio-button-checked"></div>
                 </li>
-                <li class="covid-news-language-item"><input type="radio" id="en-option" name="language" value="en"><label for="en-option">英語</label>
-                    <div class="check"></div>
+                <li class="covid-news-language-item"><input type="radio" class="covid-news-language-item__radio" id="en-option" name="language" value="en"><label for="en-option" class="covid-news-language-item__label">
+                        <div class="covid-news-language-item__text">America</div>
+                    </label>
+                    <div class="covid-news-language-item__check radio-button-checked"></div>
                 </li>
-                <li class="covid-news-language-item"><input type="radio" id="zh-option" name="language" value="zh"><label for="zh-option">中国語</label>
-                    <div class="check"></div>
+                <li class="covid-news-language-item"><input type="radio" class="covid-news-language-item__radio" id="zh-option" name="language" value="zh"><label for="zh-option" class="covid-news-language-item__label">
+                        <div class="covid-news-language-item__text">China</div>
+                    </label>
+                    <div class="covid-news-language-item__check radio-button-checked"></div>
                 </li>
-                <li class="covid-news-language-item"><input type="radio" id="fr-option" name="language" value="fr"><label for="fr-option">フランス語</label>
-                    <div class="check"></div>
+                <li class="covid-news-language-item"><input type="radio" class="covid-news-language-item__radio" id="fr-option" name="language" value="fr"><label for="fr-option" class="covid-news-language-item__label">
+                        <div class="covid-news-language-item__text">France</div>
+                    </label>
+                    <div class="covid-news-language-item__check radio-button-checked"></div>
                 </li>
-                <li class="covid-news-language-item"><input type="radio" id="pt-option" name="language" value="pt"><label for="pt-option">ポルトガル語</label>
-                    <div class="check"></div>
+                <li class="covid-news-language-item"><input type="radio" class="covid-news-language-item__radio" id="pt-option" name="language" value="pt"><label for="pt-option" class="covid-news-language-item__label">
+                        <div class="covid-news-language-item__text">Portugal</div>
+                    </label>
+                    <div class="covid-news-language-item__check radio-button-checked"></div>
                 </li>
-                <li class="covid-news-language-item"><input type="radio" id="de-option" name="language" value="de"><label for="de-option">ドイツ語</label>
-                    <div class="check"></div>
+                <li class="covid-news-language-item"><input type="radio" class="covid-news-language-item__radio" id="de-option" name="language" value="de"><label for="de-option" class="covid-news-language-item__label">
+                        <div class="covid-news-language-item__text">Germany</div>
+                    </label>
+                    <div class="covid-news-language-item__check radio-button-checked"></div>
                 </li>
-                <li class="covid-news-language-item"><input type="radio" id="es-option" name="language" value="es"><label for="es-option">スペイン語</label>
-                    <div class="check"></div>
+                <li class="covid-news-language-item"><input type="radio" class="covid-news-language-item__radio" id="es-option" name="language" value="es"><label for="es-option" class="covid-news-language-item__label">
+                        <div class="covid-news-language-item__text">Spain</div>
+                    </label>
+                    <div class="covid-news-language-item__check radio-button-checked"></div>
                 </li>
-                <li class="covid-news-language-item"><input type="radio" id="it-option" name="language" value="it"><label for="it-option">イタリア語</label>
-                    <div class="check"></div>
+                <li class="covid-news-language-item"><input type="radio" class="covid-news-language-item__radio" id="it-option" name="language" value="it"><label for="it-option" class="covid-news-language-item__label">
+                        <div class="covid-news-language-item__text">Italy</div>
+                    </label>
+                    <div class="covid-news-language-item__check radio-button-checked"></div>
                 </li>
-                <li class="covid-news-language-item"><input type="radio" id="ru-option" name="language" value="ru"><label for="ru-option">ロシア語</label>
-                    <div class="check"></div>
+                <li class="covid-news-language-item"><input type="radio" class="covid-news-language-item__radio" id="ru-option" name="language" value="ru"><label for="ru-option" class="covid-news-language-item__label">
+                        <div class="covid-news-language-item__text">Russia</div>
+                    </label>
+                    <div class="covid-news-language-item__check radio-button-checked"></div>
                 </li>
             </ul>
             <div class="form-group news-update-button-body">
-                <input class="news-update-button font-sm" type="submit" value="更新">
+                <input class="news-update-button font-sm" type="submit" value="Update">
             </div>
         </div>
 
--- backend/resources/views/articles/news_side_menu.blade.php
+++ backend/resources/views/articles/news_side_menu.blade.php
@@ -2,69 +2,81 @@
     <form class="form-inline" method="post" action="{{route('news.custom_index')}}">
         @csrf
         <div class="form-group headline-news-country-menu font-sm">
-            <p class="headline-news-side-menu-title font-sm">国名</p>
+            <p class="headline-news-side-menu-title font-sm">Country</p>
             <ul class="headline-news-country-list font-sm">
                 <li class="headline-news-country-item"><input type="radio" class="headline-news-country-item__radio" id="jp-option" name="country" value="jp"><label for="jp-option" class="headline-news-country-item__label">
-                        <div class="headline-news-country-item__text">日本</div>
+                        <div class="headline-news-country-item__text">Japan</div>
                     </label>
                     <div class="headline-news-country-item__check radio-button-checked"></div>
                 </li>
                 <li class="headline-news-country-item"><input type="radio" class="headline-news-country-item__radio" id="us-option" name="country" value="us"><label for="us-option" class="headline-news-country-item__label">
-                        <div class="headline-news-country-item__text">アメリカ</div>
+                        <div class="headline-news-country-item__text">America</div>
                     </label>
                     <div class="headline-news-country-item__check radio-button-checked"></div>
                 </li>
                 <li class="headline-news-country-item"><input type="radio" class="headline-news-country-item__radio" id="fr-option" name="country" value="fr"><label for="fr-option" class="headline-news-country-item__label">
-                        <div class="headline-news-country-item__text">フランス</div>
+                        <div class="headline-news-country-item__text">France</div>
                     </label>
                     <div class="headline-news-country-item__check radio-button-checked"></div>
                 </li>
                 <li class="headline-news-country-item"><input type="radio" class="headline-news-country-item__radio" id="pt-option" name="country" value="pt"><label for="pt-option" class="headline-news-country-item__label">
-                        <div class="headline-news-country-item__text">ポルトガル</div>
+                        <div class="headline-news-country-item__text">Portugal</div>
                     </label>
                     <div class="headline-news-country-item__check radio-button-checked"></div>
                 </li>
                 <li class="headline-news-country-item"><input type="radio" class="headline-news-country-item__radio" id="de-option" name="country" value="de"><label for="de-option" class="headline-news-country-item__label">
-                        <div class="headline-news-country-item__text">ドイツ</div>
+                        <div class="headline-news-country-item__text">Germany</div>
                     </label>
                     <div class="headline-news-country-item__check radio-button-checked"></div>
                 </li>
                 <li class="headline-news-country-item"><input type="radio" class="headline-news-country-item__radio" id="it-option" name="country" value="it"><label for="it-option" class="headline-news-country-item__label">
-                        <div class="headline-news-country-item__text">イタリア</div>
+                        <div class="headline-news-country-item__text">Italy</div>
                     </label>
                     <div class="headline-news-country-item__check radio-button-checked"></div>
                 </li>
                 <li class="headline-news-country-item"><input type="radio" class="headline-news-country-item__radio" id="ru-option" name="country" value="ru"><label for="ru-option" class="headline-news-country-item__label">
-                        <div class="headline-news-country-item__text">ロシア</div>
+                        <div class="headline-news-country-item__text">Russia</div>
                     </label>
                     <div class="headline-news-country-item__check radio-button-checked"></div>
                 </li>
             </ul>
         </div>
         <div class="form-group headline-news-category-menu font-sm">
-            <p class="headline-news-side-menu-title font-sm">カテゴリー</p>
+            <p class="headline-news-side-menu-title font-sm">Category</p>
             <ul class="headline-news-category-list font-sm">
-                <li class="headline-news-category-item"><input type="radio" id="general-option" name="category" value="general"><label for="general-option">一般</label>
-                    <div class="check"></div>
+                <li class="headline-news-category-item"><input type="radio" class="headline-news-category-item__radio" id="general-option" name="category" value="general"><label for="general-option" class="headline-news-category-item__label">
+                        <div class="headline-news-category-item__text">General</div>
+                    </label>
+                    <div class="headline-news-category-item__check radio-button-checked"></div>
                 </li>
-                <li class="headline-news-category-item"><input type="radio" id="entertainment-option" name="category" value="entertainment"><label for="entertainment-option">エンタメ</label>
-                    <div class="check"></div>
+                <li class="headline-news-category-item"><input type="radio" class="headline-news-category-item__radio" id="entertainment-option" name="category" value="entertainment"><label for="entertainment-option" class="headline-news-category-item__label">
+                        <div class="headline-news-category-item__text">Entertainment</div>
+                    </label>
+                    <div class="headline-news-category-item__check radio-button-checked"></div>
                 </li>
-                <li class="headline-news-category-item"><input type="radio" id="business-option" name="category" value="business"><label for="business-option">ビジネス</label>
-                    <div class="check"></div>
+                <li class="headline-news-category-item"><input type="radio" class="headline-news-category-item__radio" id="business-option" name="category" value="business"><label for="business-option" class="headline-news-category-item__label">
+                        <div class="headline-news-category-item__text">Business</div>
+                    </label>
+                    <div class="headline-news-category-item__check radio-button-checked"></div>
                 </li>
-                <li class="headline-news-category-item"><input type="radio" id="science-option" name="category" value="science"><label for="science-option">サイエンス</label>
-                    <div class="check"></div>
+                <li class="headline-news-category-item"><input type="radio" class="headline-news-category-item__radio" id="science-option" name="category" value="science"><label for="science-option" class="headline-news-category-item__label">
+                        <div class="headline-news-category-item__text">Science</div>
+                    </label>
+                    <div class="headline-news-category-item__check radio-button-checked"></div>
                 </li>
-                <li class="headline-news-category-item"><input type="radio" id="sports-option" name="category" value="sports"><label for="sports-option">スポーツ</label>
-                    <div class="check"></div>
+                <li class="headline-news-category-item"><input type="radio" class="headline-news-category-item__radio" id="sports-option" name="category" value="sports"><label for="sports-option" class="headline-news-category-item__label">
+                        <div class="headline-news-category-item__text">Sports</div>
+                    </label>
+                    <div class="headline-news-category-item__check radio-button-checked"></div>
                 </li>
-                <li class="headline-news-category-item"><input type="radio" id="technology-option" name="category" value="technology"><label for="technology-option">テクノロジー</label>
-                    <div class="check"></div>
+                <li class="headline-news-category-item"><input type="radio" class="headline-news-category-item__radio" id="technology-option" name="category" value="technology"><label for="technology-option" class="headline-news-category-item__label">
+                        <div class="headline-news-category-item__text">Technology</div>
+                    </label>
+                    <div class="headline-news-category-item__check radio-button-checked"></div>
                 </li>
             </ul>
             <div class="form-group news-update-button-body">
-                <input class="news-update-button font-sm" type="submit" value="更新">
+                <input class="news-update-button font-sm" type="submit" value="Update">
             </div>
         </div>
     </form>
