098417061ceb8dfa4e75e4835845605faf8ccc7a
kouki920 <kouki.htt.920@gmail.com>
2021-12-06 17:59:41 +0900

    [update]レイアウトの修正 #304

--- backend/resources/views/articles/news.blade.php
+++ backend/resources/views/articles/news.blade.php
@@ -3,9 +3,9 @@
         <div class="card-title news-body__title font-md"><a class="news-body__url" href="{{$data['url']}}" target=”_blank” rel="noopener noreferrer">{{$data['name']}}
             </a></div>
         @if(isset($data['thumbnail']))
-        <img src="{{$data['thumbnail']}}" class="card-img-top img-fluid img-thumbnail news-body__thumbnail" alt="NEWSAPIのサムネイル" />
+        <img src="{{$data['thumbnail']}}" class="news-body__thumbnail" alt="NEWSAPIのサムネイル" />
         @else
-        <img src="{{asset('/assets/images/noimage.png')}}" class="card-img-top img-fluid img-thumbnail news-thumbnail" alt="Noimage" />
+        <img src="{{asset('/assets/images/noimage.png')}}" class="news-body__thumbnail" alt="Noimage" />
         @endif
     </div>
 
