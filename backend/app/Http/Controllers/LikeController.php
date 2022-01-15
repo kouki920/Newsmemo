65f420ab14ec3b5d172cc7f16b920d71fedcdf96
kouki920 <kouki.htt.920@gmail.com>
2022-01-15 15:02:03 +0900

    [update]各Controllerのリファクタリング #357

--- backend/app/Http/Controllers/LikeController.php
+++ backend/app/Http/Controllers/LikeController.php
@@ -9,7 +9,9 @@ class LikeController extends Controller
 {
     /**
      * いいね機能のアクションメソッド
-     * detachで複数回いいねの対策
+     * $article->likesでarticlesモデルからlikesテーブル経由で紐付いているuserモデルのコレクションが返り
+     * detach()とattach()で新規登録と複数回いいねの対策を行う
+     * 最終的に、articlesモデルとリクエストを送信したユーザーのuserモデルを紐づけるlikesテーブルのレコードを新規登録する
      *
      * @param \Illuminate\Http\Request $request
      * @param \App\Models\Article $article
@@ -20,6 +22,7 @@ class LikeController extends Controller
         $article->likes()->detach($request->user()->id);
         $article->likes()->attach($request->user()->id);
 
+        // 非同期通信に対するレスポンス(JSON形式に変換されてレスポンス)
         return [
             'id' => $article->id,
             'countLikes' => $article->count_likes,
@@ -38,6 +41,7 @@ class LikeController extends Controller
     {
         $article->likes()->detach($request->user()->id);
 
+        // 非同期通信に対するレスポンス(JSON形式に変換されてレスポンス)
         return [
             'id' => $article->id,
             'countLikes' => $article->count_likes,
